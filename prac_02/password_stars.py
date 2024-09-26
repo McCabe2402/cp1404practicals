dbef22cc5e2a077fa535452b5ff80691528befea
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-09-27 03:31:53 +1000

    Refactor program to fit inside main().

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,11 @@
-password = input('Enter a password: ')
-MIN_LENGTH = 8
-while len(password) < MIN_LENGTH:
-    print('Password must be at least 8 characters long.')
+def main():
     password = input('Enter a password: ')
+    MIN_LENGTH = 8
+    while len(password) < MIN_LENGTH:
+        print('Password must be at least 8 characters long.')
+        password = input('Enter a password: ')
 
-print("*" * len(password))
+    print("*" * len(password))
+
+if __name__ == '__main__':
+    main()
