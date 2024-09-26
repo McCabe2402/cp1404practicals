73e8b485842246b1fd10d57454c6575b69866c6b
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-09-27 03:26:37 +1000

    Add password stars file.

---
+++ prac_02/password_stars.py
@@ -0,0 +1,7 @@
+password = input('Enter a password: ')
+MIN_LENGTH = 8
+while len(password) < MIN_LENGTH:
+    print('Password must be at least 8 characters long.')
+    password = input('Enter a password: ')
+
+print("*" * len(password))
