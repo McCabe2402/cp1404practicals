1dd728b9c49feb3f9d9927ec32a9ac4f27523aa8
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-09-27 03:54:08 +1000

    Add file to prac02

---
+++ prac_02/score.py
@@ -0,0 +1,22 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+# Code Fixed
+
+score = float(input("Enter score: "))
+# Changed to format both possible out of bounds.
+if score < 0 or score > 100:
+    print("Invalid score")
+else:
+    # Removed if, if, if and reformatted to if, elif, else
+    if score > 90:
+        print("Excellent")
+    elif score > 50:
+        print("Passable")
+    # Changed Bad to Fail.
+    else:
+        print("Fail")
+
+
