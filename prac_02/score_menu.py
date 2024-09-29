5f9547c97057dd93f7e0b2d2b54537c263d56fb9
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-09-29 18:03:19 +1000

    Add print function and main function. Call main function. Change variables

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,9 +1,31 @@
 import score
 
+
+def main():
+    """Main program function"""
+    print("Welcome to the Score Processing Program!")
+    # Get initial valid score
+    score = get_valid_score()
+    # Menu loop condition
+    quit_program = False
+    # Main menu loop
+    # Make function for display_menu(): to fit in with choice.
+    while choice != "Q":
+        # Make Display function here (Replace this comment with it)
+        choice = input("Enter your choice: ").upper()
+
 def get_valid_score():
     """Prompt the user for a valid score between 0 and 100 inclusive"""
-    score = int(input("Enter a score between 0 and 100: "))
-    while score < 0 or score > 100:
+    valid_score = int(input("Enter a score between 0 and 100: "))
+    while valid_score < 0 or valid_score > 100:
         print("Score must be between 0 and 100")
-        score = int(input("Enter a score between 0 and 100: "))
-    return score
+        valid_score = int(input("Enter a score between 0 and 100: "))
+    return valid_score
+
+def print_result(user_score):
+    """Print the result bases on the score"""
+    result = score.evaluate_score(user_score)
+    print(f"Result for {user_score}: {result}")
+
+if __name__ == "__main__":
+    main()
