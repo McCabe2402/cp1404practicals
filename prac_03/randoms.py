3462b8120c570885b3f70e0929279a15e97bca3b
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-10-06 18:03:10 +1000

    Add random number from 1 to 100. Remove code from before.

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,9 +1,5 @@
 import random
 
-print(random.randint(5, 20))  # line 1
-print(random.randrange(3, 10, 2))  # line 2
-print(random.uniform(2.5, 5.5))  # line 3
-
 """What did you see on line 1?"""
 # I saw various numbers like 6, 8, 10 and so on.
 """What did you see on line 2?"""
@@ -11,3 +7,5 @@ print(random.uniform(2.5, 5.5))  # line 3
 """What did you see on line 3?"""
 # I saw large decimal numbers ranging from 2.5 to 5.5
 
+random_number = random.randint(1, 100)
+print(random_number)
