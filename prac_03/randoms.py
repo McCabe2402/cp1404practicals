54eb90ccb21ec46ae5dc2a3d8f24a201cf5137f1
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-10-06 18:01:47 +1000

    Answer all questions

---
+++ prac_03/randoms.py
@@ -0,0 +1,13 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+"""What did you see on line 1?"""
+# I saw various numbers like 6, 8, 10 and so on.
+"""What did you see on line 2?"""
+# I saw 3, 5, 9 and 7
+"""What did you see on line 3?"""
+# I saw large decimal numbers ranging from 2.5 to 5.5
+
