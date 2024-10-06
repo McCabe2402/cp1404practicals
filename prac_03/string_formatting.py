c0a51287365e2b57f98e388451d772c709ff0b94
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-10-06 17:58:15 +1000

    Finish

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -1,9 +1,9 @@
-# guitar = "Gibson L-5 CES"
-# year = 1922
-# price = 16036.0
-#
-# formatted_string = f"{year} {guitar} for about ${price:,.2f}!"
-# print(formatted_string)
+guitar = "Gibson L-5 CES"
+year = 1922
+price = 16036.0
+
+formatted_string = f"{year} {guitar} for about ${price:,.2f}!"
+print(formatted_string)
 
 for i in range(11):
     result = 2 ** i
