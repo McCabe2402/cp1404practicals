6756a00e9d83281d3f6ba5a58f82e23f2f1961be
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-10-13 14:12:06 +1000

    Refactor load_data function to return a nested list of subject data.

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -13,17 +13,15 @@ def main():
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(",") # Remove the \n
+        parts[2] = int(parts[2]) # Make the number an integer
+        data.append(parts) #Append the parts for the data list
     input_file.close()
+    return data # Return the nested list
 
 
 main()
