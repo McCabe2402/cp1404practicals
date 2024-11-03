17d3cdb75350aaf100648de410abd915e75501f7
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-11-03 14:55:31 +1000

    Implement ProgrammingLanguage class and client code in languages.py

---
+++ prac_06/languages.py
@@ -0,0 +1,24 @@
+"""
+CP1404 Practical
+Programming Language code
+"""
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    # Create ProgrammingLanguage instances
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    # Print the Python programming language details
+    print(python)
+
+    # Create a list of ProgrammingLanguage objects
+    languages = [python, ruby, visual_basic]
+    print("The dynamically created languages are:")
+    for lang in languages:
+        print(lang)
+
+if __name__ == "__main__":
+    main()
---
+++ prac_06/programming_language.py
@@ -0,0 +1,24 @@
+"""
+Subject: CP1404
+Name of file: programming_language.py
+Estimate time taken:
+Actual time taken:
+
+"""
+
+class ProgrammingLanguage:
+    """Represents a programming language with its typing, reflection, and creation year"""
+    def __init__(self, name, typing, reflection, year):
+        """Initializes the programming language"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        """Returns a string representation of the programming language"""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Checks if the programming language is dynamically typed"""
+        return self.typing.lower() == "Dynamic"
