adb845d413ca29bb04b5173710ea30787767dfff
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-11-03 14:56:30 +1000

    Remove comments

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -6,15 +6,12 @@ from prac_06.programming_language import ProgrammingLanguage
 
 
 def main():
-    # Create ProgrammingLanguage instances
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
-    # Print the Python programming language details
     print(python)
 
-    # Create a list of ProgrammingLanguage objects
     languages = [python, ruby, visual_basic]
     print("The dynamically created languages are:")
     for lang in languages:
