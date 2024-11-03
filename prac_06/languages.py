f6ae6ff1299901bddd3b8e06629e1575a453ef88
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-11-03 14:56:01 +1000

    Fix missing part of code

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -18,7 +18,8 @@ def main():
     languages = [python, ruby, visual_basic]
     print("The dynamically created languages are:")
     for lang in languages:
-        print(lang)
+        if lang.is_dynamic():
+            print(lang)
 
 if __name__ == "__main__":
     main()
