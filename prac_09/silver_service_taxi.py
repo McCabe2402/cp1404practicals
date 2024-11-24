c2a55dd77702531fb6a2a0eb1499364cf33ddbd7
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-11-24 17:29:14 +1000

    Override fare calculation to include flagfall in SilverServiceTaxi

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -1,7 +1,9 @@
 from taxi import Taxi
 
+
 class SilverServiceTaxi(Taxi):
     """Specialized version of a Taxi that includes fanciness and flagfall"""
+
     flagfall = 4.50
 
     def __init__(self, name, fuel, fanciness):
@@ -9,3 +11,7 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
+
+    def get_fare(self):
+        """Return the price for the taxi trip, including the flagfall"""
+        return super().get_fare() + SilverServiceTaxi.flagfall
