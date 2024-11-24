637fb76cc527aad7d8ac3e7130da3e28034de9df
McCabe2402 <mccabe.smith@my.jcu.edu.au>
2024-11-24 16:05:04 +1000

    Add client script to test Taxi class functionality

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,2 +1,11 @@
+from prac_09.taxi import Taxi
 
+my_taxi = Taxi("Prius 1", fuel=100, price_per_km=1.23)
+my_taxi.drive(40)
+print(my_taxi)
+print(f"Current fare: ${my_taxi.get_fare():.2f}")
 
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(my_taxi)
+print(f"Current fare: ${my_taxi.get_fare():.2f}")
